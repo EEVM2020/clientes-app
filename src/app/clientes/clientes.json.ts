206109e901bdea3b40aeeddd1482b85152c59c72
EEVM2020 <eevalenciano@misena.edu.co>
2023-01-25 20:23:09 -0500

    Primer commit

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,6 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES:Cliente[]=[
+    {id:1,nombre:'Emanuel',apellido:"Valenciano",correo:'correo@gmail.com',creacion:'23/01/2023'},
+    {id:1,nombre:'Esli',apellido:"Mesa",correo:'correo12@gmail.com',creacion:'11/10/2022'}
+  ];
